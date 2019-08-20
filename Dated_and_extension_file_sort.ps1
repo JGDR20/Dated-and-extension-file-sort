b8fdaf9c2d7c8808771c18120c51a124f0ec91fa
jgdr20 <jgdr20@gmail.com>
2019-08-21 00:59:23 +0100

    Removed config.xml, replaced with hashtables

--- Dated_and_extension_file_sort.ps1
+++ Dated_and_extension_file_sort.ps1
@@ -1,96 +1,97 @@
-### CHANGE DEFAULTS IN .\config.xml!!!
+### CHANGE DEFAULTS in Default Values hashtable below!!!
 ## Parameters for $daysOldest, $daysNewest (counting back from now)
 ## $description name (allows blank/null)
-## $source and $destination directories ## CHANGE ME ##
+## $source and $destination directories
 ## $include extension filter e.g. *.jpg, *.avi
 [CmdletBinding()]
 Param(
 	[Parameter(Mandatory=$True)][AllowNull()][int]$daysOldest,
 	[Parameter(Mandatory=$True)][AllowNull()][int]$daysNewest,
 	[Parameter(Mandatory=$True)][AllowEmptyString()][string]$description,
-    [Parameter(Mandatory=$True)][AllowEmptyString()][string]$source,
-    [Parameter(Mandatory=$True)][AllowEmptyString()][string]$destination,
-    [Parameter(Mandatory=$True)][AllowEmptyString()][string]$include
+	[Parameter(Mandatory=$True)][AllowEmptyString()][string]$source,
+	[Parameter(Mandatory=$True)][AllowEmptyString()][string]$destination,
+	[Parameter(Mandatory=$True)][AllowEmptyString()][string]$include
 )
 
 ## Stop running the script if any errors occur
 $ErrorActionPreference = "Stop"
 
-## Function Get-ScriptDirectory to find the directory of the script
-function Get-ScriptDirectory
-{
-    Split-Path $script:MyInvocation.MyCommand.Path
+## Default Values hashtable
+## CHANGE ME ##
+$defaultVals = @{
+	"daysOldest" = 0;
+	"daysNewest" = 0;
+	"description" = "";
+	"source" = "D:\User\Documents\Some\Source";
+	"destination" = "D:\User\Documents\Some\Destination";
+	"include" = "*"
 }
 
-## Get config values from .\config.xml
-$config = ([xml](Get-Content (Join-Path -Path (Get-ScriptDirectory) -ChildPath config.xml))).config
-
-## Check and load variables from config if blank/empty
-# Days
-If (!($daysOldest)) {
-    $daysOldest = $config.daysOldest
-}
-If (!($daysNewest)) {
-    $daysNewest = $config.daysNewest
+## Running Values hashtable
+$runningVals = @{
+	"daysOldest" = $daysOldest;
+	"daysNewest" = $daysNewest;
+	"description" = $description;
+	"source" = $source;
+	"destination" = $destination;
+	"include" = $include
 }
 
-# Set $description
-If (!($description)) {
-    $description = $config.description
+## Functions:
+# Get-ScriptDirectory to find the directory of the script
+function Get-ScriptDirectory {
+	Split-Path $script:MyInvocation.MyCommand.Path
 }
 
-# Check if .\config.xml is populated with paths
-If (!($config.source -and $config.destination)) {
-	Read-Host -Prompt "Blank source or destination in .\config.xml, press Enter to exit"
-	exit
-}
-# Locations; $source = Source directory, $destination = Parent destination directory
-If (!($source)) {
-    $source = Resolve-Path($config.source)
-} Else {
-    $source = Resolve-Path($source)
-}
-If (!($destination)) {
-    $destination = Resolve-Path($config.destination)
-} Else {
-    $destination = Resolve-Path($destination)
+## Prepare running variables with prompt values if present, otherwise defaults
+function Running-Variables ($promptVal, $defaultVal) {
+	If ($promptVal) {
+		$running = $promptVal
+	} Else {
+		$running = $defaultVal
+	}
+	return $running
 }
 
-# File filter; $include
-If (!($include)) {
-    $include = $config.include
+# Check and load variables from $defaultVals into $runningVals if $runningVals is blank/empty for each key
+ForEach ($key in $defaultVals.Keys) {
+	$runningVals[$key] = Running-Variables -promptVal $runningVals[$key] -defaultVal $defaultVals[$key]
 }
+
+# Locations validity check
+$runningVals.source = Resolve-Path($runningVals.source)
+$runningVals.destination = Resolve-Path($runningVals.destination)
 
 ## Dates to copmpare
 $Today = (Get-Date).Date
-$StartDate = $Today.AddDays(-$daysOldest)
-$EndDate = $Today.AddDays(-$daysNewest)
+$StartDate = $Today.AddDays(-$runningVals.daysOldest)
+$EndDate = $Today.AddDays(-$runningVals.daysNewest)
 # Check if date range is valid
-If ($daysOldest -lt $daysNewest) {
-    Read-Host -Prompt "Invalid date, press Enter to exit"
+If ($runningVals.daysOldest -lt $runningVals.daysNewest) {
+	Read-Host -Prompt "Invalid date range, the oldest date is after the newest date. Press Enter to exit"
 	exit
 }
 
 ## Prepend " - " to $description if populated, else leave blank
-If ($description) {
-    $description = " - " + $description
+If ($runningVals.description) {
+	$runningVals.description = " - " + $runningVals.description
 }
 
 ## Output the settings to the user
 Write-Host Copying files between ($StartDate).ToString("yyyy-MM-dd") and ($EndDate).ToString("yyyy-MM-dd")
-Write-Host From $source to $destination
-Write-Host Using these extensions only: $include
+Write-Host From  $runningVals.source to $runningVals.destination
+Write-Host Using these extensions only: $runningVals.include
 
 # Find all '-include'ed files in $source, created within the date range specified
 # Add -recursive to Get-ChildItem in the below line (before the | ) to search subdirectories
-Get-ChildItem $source\* -Include $include |
+Get-ChildItem "$($runningVals.source)\*" -Include $runningVals.include |
 	# Compare the creation dates only (remove time - e.g. 2001-03-24 06:00:00 becomes 2001-03-24)
 	Where-Object {($_.CreationTime).Date -ge $StartDate `
 			-and ($_.CreationTime).Date -le $EndDate} |
 	# For each file found...
 	ForEach-Object {
-		# Create a destination folder ($Destination\date - $description\file extension
-		$Folder = Join-Path -Path $Destination -ChildPath (($_.CreationTime).ToString("yyyy-MM-dd") + $description)
+		# Create a destination folder (destination\date - description\file extension
+		$Folder = Join-Path -Path $runningVals.destination -ChildPath (($_.CreationTime).ToString("yyyy-MM-dd") + $runningVals.description)
 		$Folder = Join-Path -Path $Folder -ChildPath ($_.Extension).Substring(1).ToUpper()
 		$File = Join-Path -Path $Folder -ChildPath $_.Name
 		
@@ -111,6 +112,10 @@ Get-ChildItem $source\* -Include $include |
 			Copy-Item -Path $_.FullName -Destination $Folder
 			}
 		}
+
+## Clean-up
+Remove-Variable -name defaultVals
+Remove-Variable -name runningVals
 
 Write-Host Finished
 Read-Host -Prompt "Press Enter to exit"
