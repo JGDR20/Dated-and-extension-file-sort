becb8d918f2f7e172a19eb5904db191ffb129ae1
jgdr20 <jgdr20@gmail.com>
2019-08-26 22:01:24 +0100

    Removed vestigial Get-ScriptDirectory() function, moved daterange and directory validation to paramters, added  and  to total files processed, cleaned innapropriate quotation marks

--- Dated_and_extension_file_sort.ps1
+++ Dated_and_extension_file_sort.ps1
@@ -6,12 +6,39 @@
 ##	# example: include[0]: *.jpg - include[1]: *.avi
 [CmdletBinding()]
 Param(
-	[Parameter(Mandatory=$True)][AllowNull()][int]$daysOldest,
-	[Parameter(Mandatory=$True)][AllowNull()][int]$daysNewest,
-	[Parameter(Mandatory=$True)][AllowEmptyString()][string]$description,
-	[Parameter(Mandatory=$True)][AllowEmptyString()][string]$source,
-	[Parameter(Mandatory=$True)][AllowEmptyString()][string]$destination,
-	[Parameter(Mandatory=$True)][AllowEmptyCollection()][array]$include
+		[Parameter(Mandatory=$True)]
+		[AllowNull()]
+		[ValidateScript({$_ -ge 0})]
+		[int]
+		$daysOldest
+	,
+		[Parameter(Mandatory=$True)]
+		[AllowNull()]
+		[ValidateScript({$_ -ge 0 -and $_ -le $daysOldest})]
+		[int]
+		$daysNewest
+	,
+		[Parameter(Mandatory=$True)]
+		[AllowEmptyString()]
+		[string]
+		$description
+	,
+		[Parameter(Mandatory=$True)]
+		[AllowEmptyString()]
+		[ValidateScript({If($_) {Test-Path $_} Else {$true}})]
+		[string]
+		$source
+	,
+		[Parameter(Mandatory=$True)]
+		[AllowEmptyString()]
+		[ValidateScript({If($_) {Test-Path $_} Else {$true}})]
+		[string]
+		$destination
+	,
+		[Parameter(Mandatory=$True)]
+		[AllowEmptyCollection()]
+		[string[]]
+		$include
 )
 
 ## Stop running the script if any errors occur
@@ -20,33 +47,28 @@ $ErrorActionPreference = "Stop"
 ## Default Values hashtable
 ## CHANGE ME ##
 $defaultVals = @{
-	"daysOldest" = 0;
-	"daysNewest" = 0;
-	"description" = "";
-	"source" = "D:\User\Documents\Some\Source";
-	"destination" = "D:\User\Documents\Some\Destination";
-	"include" = @(
-		"*"
+	daysOldest = 0;
+	daysNewest = 0;
+	description = '';
+	source = 'D:\User\Documents\Some\Source';
+	destination = 'D:\User\Documents\Some\Destination';
+	include = @(
+		'*'
 	)
 }
 
 ## Running Values hashtable
 $runningVals = @{
-	"daysOldest" = $daysOldest;
-	"daysNewest" = $daysNewest;
-	"description" = $description;
-	"source" = $source;
-	"destination" = $destination;
-	"include" = $include
+	daysOldest = $daysOldest;
+	daysNewest = $daysNewest;
+	description = $description;
+	source = $source;
+	destination = $destination;
+	include = $include
 }
 
 ## Functions:
-# Get-ScriptDirectory to find the directory of the script
-function Get-ScriptDirectory {
-	Split-Path $script:MyInvocation.MyCommand.Path
-}
-
-## Prepare running variables with prompt values if present, otherwise defaults
+# Prepare running variables with prompt values if present, otherwise defaults
 function Running-Variables ($promptVal, $defaultVal) {
 	If ($promptVal) {
 		$running = $promptVal
@@ -61,19 +83,14 @@ ForEach ($key in $defaultVals.Keys) {
 	$runningVals[$key] = Running-Variables -promptVal $runningVals[$key] -defaultVal $defaultVals[$key]
 }
 
-# Locations validity check
-$runningVals.source = Resolve-Path($runningVals.source)
-$runningVals.destination = Resolve-Path($runningVals.destination)
+## Counters to output number of files copied and skipped
+[int]$copyCounter = 0
+[int]$skipCounter = 0
 
 ## Dates to copmpare
 $Today = (Get-Date).Date
 $StartDate = $Today.AddDays(-$runningVals.daysOldest)
 $EndDate = $Today.AddDays(-$runningVals.daysNewest)
-# Check if date range is valid
-If ($runningVals.daysOldest -lt $runningVals.daysNewest) {
-	Read-Host -Prompt "Invalid date range, the oldest date is after the newest date. Press Enter to exit"
-	exit
-}
 
 ## Prepend " - " to $description if populated, else leave blank
 If ($runningVals.description) {
@@ -82,7 +99,10 @@ If ($runningVals.description) {
 
 ## Output the settings to the user
 Write-Host Copying files between ($StartDate).ToString("yyyy-MM-dd") and ($EndDate).ToString("yyyy-MM-dd")
-Write-Host From  $runningVals.source to $runningVals.destination
+Write-Host From $runningVals.source to $runningVals.destination
+If ($runningVals.description) {
+	Write-Host "Adding `"$($runningVals.description)`" to the dated folders"
+}
 Write-Host Using these extensions only: $runningVals.include
 
 # Find all '-include'ed files in $source, created within the date range specified
@@ -108,11 +128,13 @@ Get-ChildItem -Path "$($runningVals.source)\*" -Include $runningVals.include |
 		If (Test-Path -Path $File)
 			{
 			Write-Host [-] $_.Name exists in $Folder\
+			$Script:skipCounter++
 			}
 		Else
 			{
 			Write-Host [+] Copying: $_.Name to $Folder\
 			Copy-Item -Path $_.FullName -Destination $Folder
+			$Script:copyCounter++
 			}
 		}
 
@@ -120,5 +142,5 @@ Get-ChildItem -Path "$($runningVals.source)\*" -Include $runningVals.include |
 Remove-Variable -name defaultVals
 Remove-Variable -name runningVals
 
-Write-Host Finished
-Read-Host -Prompt "Press Enter to exit"
+Write-Host "Finished copying $copyCounter files, skipped $skipCounter"
+Read-Host -Prompt 'Press Enter to exit'
